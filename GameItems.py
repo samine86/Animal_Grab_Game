81bb107f6ef6f47ee9282465c5ce4061115d5c15
samine86 <59618959+samine86@users.noreply.github.com>
2020-02-04 12:00:06 -0800

    Update GameItems.py

--- GameItems.py
+++ GameItems.py
@@ -1,5 +1,6 @@
 import math
 import random
+import Public
 
 
 class Animal:
@@ -8,21 +9,22 @@ class Animal:
         self.pos_y = _pos_y
         self.size = _size
         self.pic = _pic
+        self.grab = 0
 
         self.delta_x = 0
         self.delta_y = 0
 
         while self.delta_x == 0 and self.delta_y == 0:
-
-            self.delta_x = random.randint(-5, 5)
-            self.delta_y = random.randint(-5, 5)
+            self.delta_x = random.randint(-Public.game_speed, Public.game_speed)
+            self.delta_y = random.randint(-Public.game_speed, Public.game_speed)
 
     def calc_new_pos(self):
         self.pos_x += self.delta_x
         self.pos_y += self.delta_y
 
     def is_click_contain(self, _touch_x: int, _touch_y: int) -> bool:
-        return ( self.pos_x <= _touch_x  <= self.pos_x + self.size ) and (self.pos_y <= _touch_y <= self.pos_y + self.size)
+        return (self.pos_x + self.size >= self.pos_x <= _touch_x) and (self.pos_y <= _touch_y <= self.pos_y + self.size)
+
 
 class Animals:
     def __init__(self):
